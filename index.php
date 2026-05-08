317b66de708714c13243b6a5451be5a23872d1a0
DGWebdev2 <alan.bilik8@gmail.com>
2026-05-08 18:29:52 +0100

    php devcontainer init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DielanGarve Placeholder</title>
+</head>
+<body>
+    <h1>
+        This is a placeholder. Click the link to go to our actual website.
+    </h1>
+    <p>
+        <a href="www.dielanwebdev.pt">Dielan</a>
+    </p>
+</body>
+</html>
